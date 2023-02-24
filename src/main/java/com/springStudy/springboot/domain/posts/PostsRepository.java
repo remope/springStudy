3deffbcd2a68887c4bd66618a7b0552db8114ae3
seongjin <nature9545@gmail.com>
2023-02-24 23:57:52 +0900

    jpa 사용 설정 및 Post와 PostRepository 생성, 테스트

---
+++ src/main/java/com/springStudy/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.springStudy.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
