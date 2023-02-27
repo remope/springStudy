18f80c2f22030fe0bb69d42a104dd088444ca073
seongjin <nature9545@gmail.com>
2023-02-27 21:50:47 +0900

    등록 api 생성

---
+++ src/main/java/com/springStudy/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.springStudy.springboot.web.dto;
+
+import com.springStudy.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String auther;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String auther){
+        this.title = title;
+        this.content = content;
+        this.auther = auther;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .auther(auther)
+                .build();
+    }
+}
